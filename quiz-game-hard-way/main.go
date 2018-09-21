469e7d7f3995f1d20d65e22d51efeeb004b868e7
Tolga Boztuna <tboztuna@yahoo.com>
2018-09-21 16:49:23 +0300

    quiz game main functions

--- quiz-game-hard-way/main.go
+++ quiz-game-hard-way/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the file: %s\n", csvFileName))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse provided file.")
+	}
+
+	problems := parseLines(lines)
+
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+type problem struct {
+	question string
+	answer   string
 }
